b80394f527233d7cfad35853833337fb0e1d6640
Henry Chen <andelius@gmail.com>
2014-03-22 14:17:32 -0700

    Big update: Includes new styling. Remove numbered zones and adds a toggleable ruler instead. The rocks can toggle opacity on click. Turn the sheet vertically from horizontal. Add google font.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
 $(document).ready(function() {
-    $('.rock').draggable();
+    $(".js_draggable").draggable();
+    $(".rock").click(function() {
+        $(this).addClass("selected");
+    });
+    $(".rock.selected").click(function() {
+        $(this).removeClass("selected");
+    });
+    if ($(".ruler").hasClass("hide") === true) {
+        $(".js_ruler_toggle").click(function() {
+           $(".ruler").removeClass("hide");
+        });
+    } else {
+        $(".js_ruler_toggle").click(function() {
+           $(".ruler").addClass("hide");
+        });
+    }
+});
+$(document).click(function() {
+    $(".rock").click(function() {
+        $(this).addClass("selected");
+    });
+    $(".rock.selected").click(function() {
+        $(this).removeClass("selected");
+    });
+    if ($(".ruler").hasClass("hide") === true) {
+        $(".js_ruler_toggle").click(function() {
+            $(".ruler").removeClass("hide");
+        });
+    } else {
+        $(".js_ruler_toggle").click(function() {
+            $(".ruler").addClass("hide");
+        });
+    }
 });
